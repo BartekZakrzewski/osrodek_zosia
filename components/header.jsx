d1bb979c957df88bd96736f6542e4ae2004c0121
BartekZakrzewski <bartek.zakrzewski3012@gmail.com>
2023-04-12 21:19:32 +0200

    Commit #1

---
+++ components/header.jsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react'
+import styles from '../styles/Header.module.css'
+import { HiMenuAlt4, HiX } from "react-icons/hi"
+
+const Header = () => {
+    const navs = [
+        {
+            text: "O nas",
+            href: "#o-nas"
+        },
+        {
+            text: "Dojazd",
+            href: "#dojazd"
+        },
+        {
+            text: "Pokoje",
+            href: "#pokoje" 
+        },
+        {
+            text: "Rezerwacja",
+            href: "#rezerwacja" 
+        },
+        {
+            text: "Opinie",
+            href: "#opinie"
+        },
+        {
+            text: "Galeria",
+            href: "#galeria"
+        },
+        {
+            text: "Kontakt",
+            href: "#kontakt"
+        }
+    ]
+
+    const [isOpen, setOpen] = useState(false);
+
+    return (
+    <header className={styles.header}>
+        <div className={styles.container}>
+            <div className={styles.logo} />
+
+            <button className={`${styles.openMenuIcon} ${styles.btn}`} onClick={() => setOpen(!isOpen)}>
+                <HiMenuAlt4 />
+            </button>
+
+            <nav className={`${styles.navContainer} ${isOpen && styles.isOpen}`}>
+                <button className={`${styles.closeMenuIcon} ${styles.btn}`} onClick={() => setOpen(false)}>
+                    <HiX />
+                </button>
+                <ul className={styles.navItems}>
+                    {navs.map((dest, index) => (
+                        <li className={styles.navItem} key={index}>
+                            <a href={dest.href} className={styles.navLink}>{dest.text}</a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    </header>
+    )
+}
+
+export default Header
