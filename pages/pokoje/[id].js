de5503246954b1102f7571bd1803c8c23abab972
BartekZakrzewski <bartek.zakrzewski3012@gmail.com>
2023-04-14 23:59:52 +0200

    ...

--- pages/pokoje/[id].js
+++ pages/pokoje/[id].js
@@ -1,48 +1,48 @@
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/router'
+// import React, { useEffect } from 'react'
+// import { useRouter } from 'next/router'
 
-const Room = ({ room }) => {
-    const router = useRouter();
-    const { id } = router.query
+// const Room = ({ room }) => {
+//     const router = useRouter();
+//     const { id } = router.query
 
-    return (
-        <div>
-            { id } { room.id } { room.photo } { room.title } { room.n_people } { room.desc }
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             { id } { room.id } { room.photo } { room.title } { room.n_people } { room.desc }
+//         </div>
+//     )
+// }
 
-export default Room
+// export default Room
 
-export async function getStaticProps({ params }){
-    const req = await fetch(`https://villazosia.vercel.app//pokoje.json`)
-    const data = await req.json()
+// export async function getStaticProps({ params }){
+//     const req = await fetch(`https://villazosia.vercel.app//pokoje.json`)
+//     const data = await req.json()
 
-    const _id = params.id
-    const d = JSON.parse(data[toString(_id)])
-    return {
-        props: { 
-            room: {
-                id: d.id,
-                photo: d.photo,
-                title: d.title,
-                n_people: d.n_people,
-                desc: d.desc
-            }
-        }
-    }
-}
+//     const _id = params.id
+//     const d = JSON.parse(data[toString(_id)])
+//     return {
+//         props: { 
+//             room: {
+//                 id: d.id,
+//                 photo: d.photo,
+//                 title: d.title,
+//                 n_people: d.n_people,
+//                 desc: d.desc
+//             }
+//         }
+//     }
+// }
 
-export async function getStaticPaths() {
-    const req = await fetch('https://villazosia.vercel.app//pokoje_titles.json')
-    const data = await req.json()
+// export async function getStaticPaths() {
+//     const req = await fetch('https://villazosia.vercel.app//pokoje_titles.json')
+//     const data = await req.json()
 
-    const paths = data.map((room) => {
-        return { params: { id: toString(room) }}
-    })
+//     const paths = data.map((room) => {
+//         return { params: { id: toString(room) }}
+//     })
 
-    return {
-        paths,
-        fallback: false
-    }
-}
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
